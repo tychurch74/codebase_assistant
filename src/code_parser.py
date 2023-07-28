0fdb763c540f7a10653d83c2bce46a096dc79573
Ty Church <tychurch74@gmail.com>
2023-07-27 23:08:09 -0400

    refact: semantic search

--- src/code_parser.py
+++ src/code_parser.py
@@ -1,68 +1,115 @@
-
-import ast
-import astunparse
-import json
 import os
+import ast
+import json
+import astunparse
 from gpt_funcs import code_to_nl
-from db_gen import main as create_db
+from db_gen import db_gen as create_db
 
-# Define a function to extract docstrings and AST dumps for functions and classes.
+
 def extract_info(node):
+    """
+    Extract docstrings and AST dumps for functions and classes.
+
+    Args:
+        node (ast.AST): An AST node.
+
+    Returns:
+        dict: A dictionary containing natural language description, docstring, AST output, and source code
+              if the node is an instance of ast.FunctionDef or ast.ClassDef; None otherwise.
+    """
     if isinstance(node, (ast.FunctionDef, ast.ClassDef)):
         docstring = ast.get_docstring(node)
         ast_output = ast.dump(node)
         source_code = astunparse.unparse(node)
-        nl_desription = code_to_nl(source_code)
-        return {"nl_description": nl_desription, "docstring": docstring, "ast_output": ast_output, "source_code": source_code}
-    else:
-        return None
+        nl_description = code_to_nl(source_code)
 
-# Define a function to parse a Python file.
+        return {
+            "nl_description": nl_description,
+            "docstring": docstring,
+            "ast_output": ast_output,
+            "source_code": source_code,
+        }
+
+
 def parse_file(file_path):
-    with open(file_path, 'r') as file:
+    """
+    Parse a Python file.
+
+    Args:
+        file_path (str): The path to the Python file.
+
+    Returns:
+        dict: A dictionary containing the module docstring and a dictionary of the functions and classes.
+    """
+    with open(file_path, "r") as file:
         source_code = file.read()
 
     module = ast.parse(source_code)
     module_docstring = ast.get_docstring(module)
 
-    functions_and_classes = {}
-    for node in module.body:
-        info = extract_info(node)
-        if info is not None:
-            functions_and_classes[node.name] = info
+    functions_and_classes = {
+        node.name: extract_info(node)
+        for node in module.body
+        if extract_info(node) is not None
+    }
 
-    return {"docstring": module_docstring, "functions_and_classes": functions_and_classes}
+    return {
+        "docstring": module_docstring,
+        "functions_and_classes": functions_and_classes,
+    }
 
-# Define a function to parse a directory.
+
 def parse_directory(directory_path):
-    modules = {}
-    for filename in os.listdir(directory_path):
-        if filename.endswith('.py'):
-            file_path = os.path.join(directory_path, filename)
-            module_name = os.path.splitext(filename)[0]
-            modules[module_name] = parse_file(file_path)
+    """
+    Parse a directory.
+
+    Args:
+        directory_path (str): The path to the directory.
+
+    Returns:
+        dict: A dictionary of the modules parsed from the directory.
+    """
+    modules = {
+        os.path.splitext(filename)[0]: parse_file(
+            os.path.join(directory_path, filename)
+        )
+        for filename in os.listdir(directory_path)
+        if filename.endswith(".py")
+    }
+
     return modules
 
+
 def parse_codebase():
-    if not os.path.exists('gpt_workspace'):
-        os.mkdir('gpt_workspace')
-    
+    """
+    Parse a codebase.
+
+    This function checks if a workspace exists, prompts the user for the project folder name and
+    directory location, creates a JSON file with this information, and creates a SQLite database from
+    the JSON file.
+
+    Returns:
+        str: The path to the database file.
+    """
+    workspace = "gpt_workspace"
+    if not os.path.exists(workspace):
+        os.mkdir(workspace)
+
     project_folder_name = input("Project folder name: ")
     directory_location = input("Directory location: ")
     modules = parse_directory(directory_location)
-    
-    data = {project_folder_name: {directory_location: modules}}
-    
-    with open(f'gpt_workspace/{project_folder_name}-{directory_location}_info.json', 'w') as file:
-        json.dump(data, file, indent=2)
-        
-    create_db(f'gpt_workspace/{project_folder_name}-{directory_location}_info.json', f'gpt_workspace/{project_folder_name}-{directory_location}_info.db')
-    
-    db_path = f'gpt_workspace/{project_folder_name}-{directory_location}_info.db'
-    return db_path
-    
-    
-if __name__ == '__main__':
-    parse_codebase()
 
-    
+    data = {project_folder_name: {directory_location: modules}}
+    json_filename = f"{workspace}/{project_folder_name}-{directory_location}_info.json"
+
+    with open(json_filename, "w") as file:
+        json.dump(data, file, indent=2)
+
+    db_filename = f"{workspace}/{project_folder_name}-{directory_location}_info.db"
+    create_db(json_filename, db_filename)
+
+    return db_filename
+
+
+if __name__ == "__main__":
+    parse_codebase()
--- src/db_gen.py
+++ src/db_gen.py
@@ -4,32 +4,49 @@ from sqlite3 import Error
 
 
 def create_connection(db_file):
-    conn = None;
+    """
+    Create a database connection to a SQLite database.
+
+    Args:
+        db_file (str): Path to the database file.
+
+    Returns:
+        Connection object or None
+    """
     try:
-        conn = sqlite3.connect(db_file) # creates a database file or connects to an existing one
+        conn = sqlite3.connect(db_file)
         return conn
     except Error as e:
         print(e)
-    
+        return None
+
 
 def create_tables(conn):
+    """
+    Create tables in the SQLite database.
+
+    Args:
+        conn (sqlite3.Connection): Connection object to the SQLite database.
+    """
     try:
         cursor = conn.cursor()
-        cursor.execute('''
+
+        tables = [
+            """
             CREATE TABLE projects (
                 id INTEGER PRIMARY KEY,
                 name TEXT NOT NULL
             );
-        ''')
-        cursor.execute('''
+            """,
+            """
             CREATE TABLE folders (
                 id INTEGER PRIMARY KEY,
                 name TEXT NOT NULL,
                 project_id INTEGER,
                 FOREIGN KEY(project_id) REFERENCES projects(id)
             );
-        ''')
-        cursor.execute('''
+            """,
+            """
             CREATE TABLE files (
                 id INTEGER PRIMARY KEY,
                 name TEXT NOT NULL,
@@ -37,8 +54,8 @@ def create_tables(conn):
                 folder_id INTEGER,
                 FOREIGN KEY(folder_id) REFERENCES folders(id)
             );
-        ''')
-        cursor.execute('''
+            """,
+            """
             CREATE TABLE functions_classes (
                 id INTEGER PRIMARY KEY,
                 name TEXT NOT NULL,
@@ -49,47 +66,90 @@ def create_tables(conn):
                 file_id INTEGER,
                 FOREIGN KEY(file_id) REFERENCES files(id)
             );
-        ''')
+            """,
+        ]
+
+        for table in tables:
+            cursor.execute(table)
     except Error as e:
         print(e)
 
 
 def parse_json_insert_data(conn, json_file_path):
-    with open(json_file_path, 'r') as f:
+    """
+    Parse JSON data and insert it into the SQLite database.
+
+    Args:
+        conn (sqlite3.Connection): Connection object to the SQLite database.
+        json_file_path (str): Path to the JSON file containing the data.
+    """
+    with open(json_file_path, "r") as f:
         data = json.load(f)
 
     cursor = conn.cursor()
 
     for project_name, folders in data.items():
-        cursor.execute('INSERT INTO projects(name) VALUES(?)', (project_name,))
+        cursor.execute("INSERT INTO projects(name) VALUES(?)", (project_name,))
         project_id = cursor.lastrowid
 
         for folder_name, files in folders.items():
-            cursor.execute('INSERT INTO folders(name, project_id) VALUES(?, ?)', (folder_name, project_id))
+            cursor.execute(
+                "INSERT INTO folders(name, project_id) VALUES(?, ?)",
+                (folder_name, project_id),
+            )
             folder_id = cursor.lastrowid
 
             for file_name, file_contents in files.items():
-                docstring = file_contents.get('docstring')
-                cursor.execute('INSERT INTO files(name, docstring, folder_id) VALUES(?, ?, ?)', (file_name, docstring, folder_id))
+                docstring = file_contents.get("docstring")
+                cursor.execute(
+                    "INSERT INTO files(name, docstring, folder_id) VALUES(?, ?, ?)",
+                    (file_name, docstring, folder_id),
+                )
                 file_id = cursor.lastrowid
 
-                for function_name, function_contents in file_contents.get('functions_and_classes', {}).items():
-                    nl_description = function_contents.get('nl_description')
-                    docstring = function_contents.get('docstring')
-                    ast_output = function_contents.get('ast_output')
-                    source_code = function_contents.get('source_code')
+                for function_name, function_contents in file_contents.get(
+                    "functions_and_classes", {}
+                ).items():
+                    nl_description = function_contents.get("nl_description")
+                    docstring = function_contents.get("docstring")
+                    ast_output = function_contents.get("ast_output")
+                    source_code = function_contents.get("source_code")
 
-                    cursor.execute('''
+                    cursor.execute(
+                        """
                         INSERT INTO functions_classes(name, nl_description, docstring, ast_output, source_code, file_id) 
                         VALUES(?, ?, ?, ?, ?, ?)
-                    ''', (function_name, nl_description, docstring, ast_output, source_code, file_id))
+                        """,
+                        (
+                            function_name,
+                            nl_description,
+                            docstring,
+                            ast_output,
+                            source_code,
+                            file_id,
+                        ),
+                    )
 
     conn.commit()
 
 
-def query_function_class(conn, project_name, folder_name, file_name, function_class_name):
+def query_function_class(
+    conn, project_name, folder_name, file_name, function_class_name
+):
+    """
+    Query a function or class from the SQLite database.
+
+    Args:
+        conn (sqlite3.Connection): Connection object to the SQLite database.
+        project_name (str): Name of the project.
+        folder_name (str): Name of the folder.
+        file_name (str): Name of the file.
+        function_class_name (str): Name of the function or class.
+    """
     cursor = conn.cursor()
-    cursor.execute('''
+
+    cursor.execute(
+        """
         SELECT 
             f_c.name, 
             f_c.nl_description, 
@@ -105,7 +165,9 @@ def query_function_class(conn, project_name, folder_name, file_name, function_cl
             f.name = ? AND
             fo.name = ? AND
             p.name = ?
-    ''', (function_class_name, file_name, folder_name, project_name))
+        """,
+        (function_class_name, file_name, folder_name, project_name),
+    )
 
     rows = cursor.fetchall()
 
@@ -113,19 +175,36 @@ def query_function_class(conn, project_name, folder_name, file_name, function_cl
         print(row)
 
 
-def main(json_file_path, db_file_path):
+def db_gen(json_file_path, db_file_path):
+    """
+    Generate a SQLite database from a JSON file.
+
+    Args:
+        json_file_path (str): Path to the JSON file.
+        db_file_path (str): Path to the SQLite database file.
+    """
     conn = create_connection(db_file_path)
-    create_tables(conn)
-    parse_json_insert_data(conn, json_file_path)
-    
+    if conn is not None:
+        create_tables(conn)
+        parse_json_insert_data(conn, json_file_path)
 
-def test_main(json_file_path, db_file_path):
+
+def test_db_gen(json_file_path, db_file_path):
+    """
+    Test the generation of a SQLite database from a JSON file.
+
+    Args:
+        json_file_path (str): Path to the JSON file.
+        db_file_path (str): Path to the SQLite database file.
+    """
     conn = create_connection(db_file_path)
-    create_tables(conn)
-    parse_json_insert_data(conn, json_file_path)
-    query_function_class(conn, 'codebase_assistant', 'src', 'parser', 'extract_info')
+    if conn is not None:
+        create_tables(conn)
+        parse_json_insert_data(conn, json_file_path)
+        query_function_class(
+            conn, "codebase_assistant", "src", "parser", "extract_info"
+        )
 
 
-if __name__ == '__main__':
-    test_main('codebase_info.json', 'codebase_info.db')
-
+if __name__ == "__main__":
+    db_gen("codebase_info.json", "codebase_info.db")
